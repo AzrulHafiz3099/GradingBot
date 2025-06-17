038ebfde4b6de9ec333c9b3c278837deea0a672e
azrul <azrulhafiz3099@gmail.com>
2025-06-18 00:28:00 +0800

    env change

--- lib/utils/env.dart
+++ lib/utils/env.dart
@@ -1,12 +1,12 @@
 class Env {
   // Backend base URL (can be your local IP or production URL)
   //localhost
-  // static const String baseUrl = 'http://192.168.0.30:8000';
-  // static const String classApi = 'http://192.168.0.30:8000/api_class';
+  static const String baseUrl = 'http://192.168.0.30:8000';
+  static const String classApi = 'http://192.168.0.30:8000/api_class';
 
   //utem server
-  static const String baseUrl = 'https://apidev.utem.edu.my/';
-  static const String classApi = 'https://apidev.utem.edu.my/api_class';
+  // static const String baseUrl = 'https://apidev.utem.edu.my/';
+  // static const String classApi = 'https://apidev.utem.edu.my/api_class';
    
   // Add other keys or URLs as needed
   static const String apiKey = 'YOUR_API_KEY_HERE';
